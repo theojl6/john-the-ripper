64b89d74eb7333d248787ce105b8b784e63005be
Teddy <leungteddy12@gmail.com>
2024-10-31 00:30:59 +0800

    add steps to plan

--- src/md5.rs
+++ src/md5.rs
@@ -10,6 +10,14 @@
 
 pub struct Digest(pub [u8; 16]);
 
-pub fn compute(data: &[u8]) -> Digest {
+pub fn compute(message: &[u8]) -> Digest {
+    // message preprocessing
+    // break message down into chunks of 512-bit blocks (sixteen 32-bit words)
+    // pad message so that it is divisible by 512
+    // to pad, append a single bit (1) to the end of the message, then append zeros until it is a multiple of 512
+    // but minus 64
+    // this space of 64 will be filled by bits representing the length of the original message, module 2^64
+    // main algorithm
+    //
     unimplemented!()
 }
